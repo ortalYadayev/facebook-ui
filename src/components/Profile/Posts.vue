15c606dddcbfb280f35ea5917a4b6c3fcda94c61
Ortal Yadayev <ortalll20111@gmail.com>
2021-12-03 14:23:01 +0200

    add like and comment of comment

--- src/components/Profile/Posts.vue
+++ src/components/Profile/Posts.vue
@@ -208,7 +208,7 @@
                     class="hover:opacity-90 bg-gray-rgb h-6 w-6 rounded-full"
                   >
                 </router-link>
-                <div class="max-w-comment flex-1 flex flex-col mx-2 break-words">
+                <div class="max-w-comment flex flex-col mx-2 break-words">
                   <div class="flex flex-col rounded-3xl bg-gray-rgb px-4 py-2">
                     <router-link
                       :to="{ name: 'Profile', params: { username: user.username } }"
@@ -221,6 +221,12 @@
                     </span>
                   </div>
                   <div class="text-xs">
+                    <button class="hover:underline">
+                      like
+                    </button>
+                    <button class="hover:underline mx-3">
+                      comment
+                    </button>
                     {{ comment.commentFormat }}
                   </div>
                 </div>
